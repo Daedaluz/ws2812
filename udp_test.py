ff7b1a9b800895ab9d45d87b81e5a897fd7520c8
Tobias Assarsson <tobias.assarsson@gmail.com>
2015-11-05 15:04:57 +0100

    test-scripts

--- udp_test.py
+++ udp_test.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python2
 import socket
+import sys
 
-IP = '192.168.9.119'
+#IP = '192.168.3.136'
+IP = '10.37.201.187'
 PORT = 50007
 
-DATA = '\x10\x00\x00\x00\x10\x00\x00\x00\x10'*16
+DATA = "".join(sys.argv[1:]).decode("hex")
+
+#DATA = '\x10\x00\x00\x00\x10\x00\x00\x00\x10'*16
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
 s.connect((IP, PORT))
 s.send(DATA)
